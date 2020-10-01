3cc4ca674a4b92de8026939186a6d4d77fa5100b
YAIR JIMENEZ <YJIMENEZ@ARKUSNEXUS.COM>
2020-10-01 09:01:25 -0500

    build renamed to docs for githubpages

---
+++ docs/precache-manifest.233493ef177ae7596a2b3f9d5e328fc1.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "97f44e181e644f05a3bb26032e84d64a",
+    "url": "/index.html"
+  },
+  {
+    "revision": "cf79a28aca9c8de99c24",
+    "url": "/static/css/main.0a4831a3.chunk.css"
+  },
+  {
+    "revision": "b6e52f149bd8d488e1fa",
+    "url": "/static/js/2.9a1784d2.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.9a1784d2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "cf79a28aca9c8de99c24",
+    "url": "/static/js/main.a2ef66d8.chunk.js"
+  },
+  {
+    "revision": "4e3a1f73b6e5de40b557",
+    "url": "/static/js/runtime-main.5c054f69.js"
+  }
+]);
